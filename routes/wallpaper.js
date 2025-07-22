9bd66f4b69a7eea1fc91098b1d09afe0ae2cc54e
Anwesha Bhatt <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-07-22 23:30:20 +0530

    Update wallpaper.js

--- routes/wallpaper.js
+++ routes/wallpaper.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const updateWallpaper = require("../controller/updateWallpaper");
 
+// ✅ Endpoint: POST /api/user/update-wallpaper
 router.post("/update-wallpaper", updateWallpaper);
 
 module.exports = router;
