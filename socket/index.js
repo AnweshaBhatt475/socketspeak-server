d6fc852ac7b06e93cde34f4525283a6389908f30
Anwesha Bhatt <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-07-22 22:49:33 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -16,7 +16,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173',
-'https://socketspeak-client.onrender.com/',
+      'https://socketspeak-client.onrender.com',
       'https://chat-app-frontend-tt7i.onrender.com'
     ],
     credentials: true,
